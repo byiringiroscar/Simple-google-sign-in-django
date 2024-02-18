7efc4168d2d349dd2e14b08d09ce1531bcb6e184
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-02-18 23:57:52 +0200

    wip button generator and views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from google.oauth2 import id_token
+from google.auth.transport import requests
+import jwt
+
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
+
+
+@csrf_exempt
+def auth_receiver(request):
+    """
+    Google calls this URL after the user has signed in with their Google account.
+    """
+    token = request.POST['credential']
+
+    try:
+        user_data = id_token.verify_oauth2_token(
+            token, requests.Request(), os.environ['GOOGLE_OAUTH_CLIENT_ID']
+        )
+    except ValueError:
+        return HttpResponse(status=403)
+
+    # In a real app, I'd also save any new user here to the database. See below for a real example I wrote for Photon Designer.
+    # You could also authenticate the user here using the details from Google (https://docs.djangoproject.com/en/4.2/topics/auth/default/#how-to-log-a-user-in)
+    request.session['user_data'] = user_data
+
+    return redirect('sign_in')
+
+
+def sign_out(request):
+    del request.session['user_data']
+    return redirect('sign_in')
